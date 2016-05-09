0ad825cacf52185de0ae3dec6a8f949dab880077
IsmaelNascimento <ismaeln.business@gmail.com>
2016-05-09 16:00:59 -0300

    Update CodigoRtcDS1307.ino

--- CodigoRtcDS1307.ino
+++ CodigoRtcDS1307.ino
@@ -2,146 +2,138 @@
 
 #include "RTClib.h"
 
-
 // Objeto rtc (relogio de tempo real)
 RTC_DS1307 rtc;
 
 //Conectar no pino digital 8 do Arduino Uno (Pode mudar o pino de entrada, mas mude tambem o numero do pino no codigo)
 int rele = 8;
 
-void setup () 
+void setup ()
 {
-     Serial.begin(9600);
-     
-     pinMode(rele, OUTPUT);
-     
-     rtc.begin();  //inicia o objeto rtc
+  Serial.begin(9600);
+  
+  // A porta do rele esta configurada para liberar energia 
+  pinMode(rele, OUTPUT);
+  
+  rtc.begin(); //inicia o objeto rtc
+  
+  //Comente a linha abaixo depois da primeira compilacao.
+  //rtc.adjust(DateTime(__DATE__, __TIME__));
 
-     // Verifica se o RTC esta funcionando
-     if (!rtc.isrunning()) 
-     {
-           Serial.print("RTC is NOT running!");
-     }
-     else
-     { 
-         rtc.adjust(DateTime(__DATE__, __TIME__)); // Ajusta o rtc para a data e hora em que o sketch foi compilado
-         Serial.print("RTC is running!");
-     }
 }
-
-
-void loop() 
+void loop()
 {
-    horasDeTocar();
+  showInformations();
+  horasDeTocar();
 }
-
 void showInformations()
 {
-    DateTime now = rtc.now();
-    
-    //Date  
-    Serial.print(now.day(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.year(), DEC);
-      
-    //Time  
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    
-    delay(1000);
+  DateTime now = rtc.now();
+  
+  //Date
+  Serial.print(now.day(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.year(), DEC);
+  
+  //Time
+  Serial.print(' ');
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+  
+  delay(1000);
 }
-
 void horasDeTocar()
 {
-  if(now.hour == 07 && now.minute >= 10) //Primeiro toque.
+  DateTime now = rtc.now();
+  
+  if(now.hour() == 7 && now.minute() == 10 && now.second() >= 0) //Primeiro toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 07 && now.minute >= 20) //Segundo toque.
+  else if(now.hour() == 7 && now.minute() == 20 && now.second() >= 0) //Segundo toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 09 && now.minute >= 00) //Terceiro toque.
+  else if(now.hour() == 9 && now.minute() == 0 && now.second() >= 0) //Terceiro toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 09 && now.minute >= 20) //Quaerto toque.
+  else if(now.hour() == 9 && now.minute() == 20 && now.second() >= 0) //Quarto toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 11 && now.minute >= 50) //Quinto toque.
+  else if(now.hour() == 11 && now.minute() == 50 && now.second() >= 0) //Quinto toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 13 && now.minute >= 10) //Sexto toque.
+  else if(now.hour() == 13 && now.minute() == 10 && now.second() >= 0) //Sexto toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 13 && now.minute >= 20) //Setimo toque.
+  else if(now.hour() == 13 && now.minute() == 20 && now.second() >= 0) //Setimo toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 15 && now.minute >= 00) //Oitavo toque.
+  else if(now.hour() == 15 && now.minute() == 0 && now.second() >= 0) //Oitavo toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 15 && now.minute >= 20) //Nono toque.
+  else if(now.hour() == 15 && now.minute() == 20 && now.second() >= 0) //Nono toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
-  else if(now.hour == 16 && now.minute >= 59 && now,second >= 30 ) //Decimo toque.
+  else if(now.hour() == 16 && now.minute() == 59 && now.second() >= 0) //Decimo toque.
   {
-    digitalWrite(rele, HIGH);
     Serial.print("Esta tocando\n");
-    delay(5000);  
+    digitalWrite(rele, HIGH);
+    //Toca 15 segundos (15 * 1000)
+    delay(15000);    
     digitalWrite(rele, LOW);
-    
   }
   else
   {
-    digitalWrite(rele, LOW);  //Rele desligado.
+    digitalWrite(rele, LOW); //Sirene desligada.
   }
 }
