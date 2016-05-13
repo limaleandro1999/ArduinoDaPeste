3bf58b5ae60d9e4c1093d81d1fed73743bdc4590
Ismael Nascimento <ismaeln.business@gmail.com>
2016-05-13 09:26:01 -0300

    Update CodigoRtcDS1307.ino

--- CodigoRtcDS1307.ino
+++ CodigoRtcDS1307.ino
@@ -10,6 +10,7 @@ RTC_DS1307 rtc;
 
 //Conectar no pino digital 8 do Arduino Uno (Pode mudar o pino de entrada, mas mude tambem o numero do pino no codigo)
 int rele = 8;
+int ledL = 13;
 
 void setup ()
 {
@@ -17,6 +18,7 @@ void setup ()
   
   // A porta do rele esta configurada para liberar energia 
   pinMode(rele, OUTPUT);
+  pinMode(ledL, OUTPUT);
   
   rtc.begin(); //inicia o objeto rtc
   
@@ -59,85 +61,114 @@ void horasDeTocar()
   if(now.hour() == 7 && now.minute() == 10 && now.second() >= 0) //Primeiro toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 7 && now.minute() == 20 && now.second() >= 0) //Segundo toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 9 && now.minute() == 0 && now.second() >= 0) //Terceiro toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 9 && now.minute() == 20 && now.second() >= 0) //Quarto toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 11 && now.minute() == 50 && now.second() >= 0) //Quinto toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 13 && now.minute() == 10 && now.second() >= 0) //Sexto toque.
   {
-    Serial.print("Esta tocando\n");
+    SSerial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 13 && now.minute() == 20 && now.second() >= 0) //Setimo toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 15 && now.minute() == 0 && now.second() >= 0) //Oitavo toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 15 && now.minute() == 20 && now.second() >= 0) //Nono toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else if(now.hour() == 16 && now.minute() == 59 && now.second() >= 0) //Decimo toque.
   {
     Serial.print("Esta tocando\n");
+    digitalWrite(ledL, HIGH);
     digitalWrite(rele, HIGH);
     //Toca 15 segundos (15 * 1000)
     delay(15000);    
     digitalWrite(rele, LOW);
+    piscaLed();
   }
   else
   {
     digitalWrite(rele, LOW); //Sirene desligada.
+    piscaLed();
   }
+}
+
+void piscaLed()
+{
+  digitalWrite(ledL, HIGH);
+  delay(500);
+  digitalWrite(ledL, LOW);
+  delay(500);
 }
